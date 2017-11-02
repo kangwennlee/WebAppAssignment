380b853f6b8c50cd24d40c25ad116cf83638eb9d
Lee Kang Wenn <kangwenn@gmail.com>
2017-11-02 22:48:38 +0800

    replaced <a> with <asp HyperLink>

--- WebApplication3/Site1.Master.designer.cs
+++ WebApplication3/Site1.Master.designer.cs
@@ -49,6 +49,15 @@ namespace WebApplication3 {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink4;
         
         /// <summary>
+        /// HyperLink5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink5;
+        
+        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
